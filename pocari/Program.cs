062737e85ea3f773bd1d8cf18906615fbd758a5b
Defriska <defriskanaura@gmail.com>
2023-11-13 11:46:18 +0700

    commit naura

---
+++ pocari/Program.cs
@@ -0,0 +1,50 @@
+﻿//General
+class Program 
+{
+	static void Main() 
+	{
+		Cat pokari = new Cat("yellow");
+		pokari.isMale = true;
+		string result = pokari.colour; 
+		Console.WriteLine(result);
+		pokari.Eat();
+		
+		Cat jiji = new Cat("black");
+		
+		Cat coki = new Cat("white");
+		
+		Cat tompel = new Cat("grey");
+	}
+}
+class Cat //Blueprint
+{
+	//Variable / Field
+	public string colour;
+	public int age;
+	public bool isMale;
+	public float weight;
+
+	//Constructor
+	public Cat(string colour) 
+	{
+		this.colour = colour;
+	}
+		
+	//Method / Function
+	public void Eat() 
+	{
+		Console.WriteLine("Eat");
+	}
+	public void Sleep() 
+	{
+		Console.WriteLine("Sleep");
+	}
+	public void Chaos() 
+	{
+		Console.WriteLine("Chaos");
+	}
+	public void Meow() 
+	{
+		Console.WriteLine("Meow");
+	}
+}
