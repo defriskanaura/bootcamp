3718e3f6b80ffdb8727b92689d0f1bdda3669526
defriskanaura <142398659+defriskanaura@users.noreply.github.com>
2023-11-17 16:09:12 +0000

    ok

--- self-train/17Nov/Answer_1/Program.cs
+++ self-train/17Nov/Answer_1/Program.cs
@@ -3,9 +3,21 @@
 		int[] inventory = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 		foreach (int items in inventory)
 		{
-			if (items % 3 == 0 || items % 5 == 0)
+			if (items==0)
 			{
-				Console.Write("Foo ");
+				Console.Write($"{items} ");
+			}
+			else if (items % 3 == 0 && !(items % 5 == 0))
+			{
+				Console.Write("foo ");
+			}
+			else if (items % 5 == 0 && !(items % 3 == 0))
+			{
+				Console.Write("bar ");
+			}
+			else if (items % 3 == 0 && items % 5 == 0 && !(items==0))
+			{
+				Console.Write("foobar ");
 			}
 			else 
 			{
