9c3376c51e1459782bd3a048f03390bd13e96f0e
defriskanaura <142398659+defriskanaura@users.noreply.github.com>
2023-11-17 16:02:44 +0000

    ol

--- self-train/17Nov/Answer_2/Program.cs
+++ self-train/17Nov/Answer_2/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 class Program {
 	static void Main(){
 		string? userInput;
@@ -9,22 +9,54 @@ class Program {
 		Console.WriteLine(myArray.Length);
 	}
 }
-// class Program 
-// {
-// 	static void Main() 
-// 	{
-// 		WordPlay foobar = new WordPlay(15, "foo");
-// 	}
-// }
-// class WordPlay 
-// {
-// 	public readonly int n = 6;
-// 	public readonly string word;
+*/
+class Program 
+{
+	static void Main() 
+	{
+		WordPlay foobar = new WordPlay(15, "foo");
+		foobar.Replace();
+	}
+}
+class WordPlay 
+{
+	public readonly int n;
+	public readonly string word;
 	
-// 	//constructor
-// 	public WordPlay(int n, string word) 
-// 	{
-// 		this.n = n;
-// 		this.word = word;
-// 	}
-// }
+	
+	//constructor
+	public WordPlay(int n, string word) 
+	{
+		this.n = n;
+		this.word = word;
+	}
+	//method
+	public void Replace()
+	{
+		int[] arrayWord = new int[n+1];
+		for (int i = 0; i < n+1; i++)
+		{
+			arrayWord[i] = i;
+		}
+		
+		foreach (int items in arrayWord)
+		{
+			if (items % 3 == 0 && !(items % 5 == 0) && !(items==0))
+			{
+				Console.Write("foo ");
+			}
+			else if (items % 5 == 0 && !(items % 3 == 0) && !(items==0))
+			{
+				Console.Write("bar ");
+			}
+			else if (items % 3 == 0 && items % 5 == 0 && !(items==0))
+			{
+				Console.Write("foobar ");
+			}
+			else 
+			{
+				Console.Write($"{items} ");
+			}
+		}
+	}
+}
