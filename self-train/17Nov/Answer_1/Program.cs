2d13681341025d487f24570e57aaec299ed77b3b
defriskanaura <142398659+defriskanaura@users.noreply.github.com>
2023-11-17 16:09:26 +0000

    ok

--- self-train/17Nov/Answer_1/Program.cs
+++ self-train/17Nov/Answer_1/Program.cs
@@ -1,6 +1,6 @@
 ﻿class Program {
 	static void Main(){
-		int[] inventory = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+		int[] inventory = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 		foreach (int items in inventory)
 		{
 			if (items==0)
@@ -15,7 +15,7 @@
 			{
 				Console.Write("bar ");
 			}
-			else if (items % 3 == 0 && items % 5 == 0 && !(items==0))
+			else if (items % 3 == 0 && items % 5 == 0)
 			{
 				Console.Write("foobar ");
 			}
