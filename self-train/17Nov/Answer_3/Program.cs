8d00bb30bc39edca65762ccd99316335608d5d54
defriskanaura <142398659+defriskanaura@users.noreply.github.com>
2023-11-17 17:28:54 +0000

    ok

--- self-train/17Nov/Answer_3/Program.cs
+++ self-train/17Nov/Answer_3/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program 
+{
+	static void Main() 
+	{
+		WordPlay foobar = new WordPlay();
+		foobar.Play();
+	}
+}
+class WordPlay 
+{
+	private string stringAngka = GetAngka();
+	//method
+	private static string GetAngka()
+	{
+	 Console.WriteLine("Masukkan banyaknya angka: ");
+	 return Console.ReadLine();
+	}
+	public void Play()
+	{
+		var n = int.Parse(stringAngka);
+		Console.WriteLine(n);
+		int[] arrayWord = new int[n+1];
+		for (int i = 0; i < n+1; i++)
+		{
+			arrayWord[i] = i;
+		}
+		
+		foreach (int items in arrayWord)
+		{
+			if (items % 3 == 0 && !(items % 5 == 0) && !(items==0))
+			{
+				Console.Write("foo ");
+			}
+			else if (items % 5 == 0 && !(items % 3 == 0) && !(items==0))
+			{
+				Console.Write("bar ");
+			}
+			else if (items % 3 == 0 && items % 5 == 0 && !(items==0))
+			{
+				Console.Write("foobar ");
+			}
+			else 
+			{
+				Console.Write($"{items} ");
+			}
+		}
+	}
+}
