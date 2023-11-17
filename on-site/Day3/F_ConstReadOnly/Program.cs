b75036e7b62e81862e90c8d67d7aed2e8d7971f7
defriskanaura <142398659+defriskanaura@users.noreply.github.com>
2023-11-17 02:12:13 +0000

    y

--- on-site/Day3/F_ConstReadOnly/Program.cs
+++ on-site/Day3/F_ConstReadOnly/Program.cs
@@ -10,18 +10,6 @@ class Program {
 }
 class Calculator {
 	public const float phi = 3.14f;
-//Const vs ReadOnly
-class Program {
-	static void Main(){
-		Calculator calc = new Calculator(10);
-		calc.g.Dump();
-		// calc.g = 11; <- it will error
-		// Const must be assigned before compilation
-		// ReadOnly can be assigned via constructor
-	}
-}
-class Calculator {
-	public const float phi = 3.14f;
 	public readonly int g;
 	public Calculator(int x) {
 		g = x;
