55b0bc7a9907ae490a2f254c656490151c696fca
Defriska Naura <defriskanaura@gmail.com>
2023-11-21 06:57:52 +0700

    ok

---
+++ self-train/16Nov/Genshin_1/Program.cs
@@ -0,0 +1,124 @@
+﻿class Program 
+{
+	static void Main() 
+	{
+		GenshinCharacter CharacterA = new();
+		CharacterA.SetName();
+		CharacterA.SetGender();
+		CharacterA.SetHeight();
+		CharacterA.SetElement();
+		CharacterA.SetWeapon();
+		CharacterA.TestCharacter();
+	}
+}
+
+public class CharacterAct
+{
+	public void ElementalSkill(string characterName) 
+	{
+		Console.WriteLine ($"{characterName} is doing elemental skill and stacking energy");	
+	}
+	public void ElementalBurst(string characterName)
+	{
+		Console.WriteLine ($"{characterName} is doing elemental burst once");
+	}
+	public void Idle(string characterName) 
+	{
+		Console.WriteLine ($"{characterName} is doing idle");	
+	}
+	public void Walk(string characterName) 
+	{
+		Console.WriteLine ($"{characterName} is walking 100 meters");	
+	}
+	public void Run(string characterName) 
+	{
+		Console.WriteLine ($"{characterName} is running 100 meters");	
+	}
+}
+
+public class GenshinCharacter : CharacterAct
+{
+	private string _name = "";
+	private string _gender = "";
+	private string _height = "";
+	private string _element = "";
+	private string _weapon = "";
+	
+	public string SetName() 
+	{
+		Console.WriteLine("Write the name of your Genshin Character : ");
+		while (_name == "")
+		{
+			_name = Console.ReadLine();
+		}
+		return _name;
+	}
+	public string GetName() 
+	{
+		return _name;
+	}
+	
+	public string SetGender() 
+	{
+		Console.WriteLine("Choose your character's gender : ");
+		Console.WriteLine("[Male]    [Female]");
+		while (_gender == "")
+		{
+			_gender = Console.ReadLine();
+		}
+		return _gender;
+	}
+	public string GetGender() 
+	{
+		return _gender;
+	}
+		
+	public string SetHeight() 
+	{
+		Console.WriteLine("Choose your character's height : ");
+		Console.WriteLine("[Tall]    [Medium]    [Short]");
+		while (_height == "")
+		{
+			_height = Console.ReadLine();
+		}
+		return _height;
+	}
+	public string GetHeight() 
+	{
+		return _height;
+	}
+			
+	public string SetElement() 
+	{
+		Console.WriteLine("Choose your character's element : ");
+		Console.WriteLine("[Anemo]    [Geo]    [Electro]    [Dendro]    [Hydro]    [Pyro]     [Cryo]");
+		while (_element == "")
+		{
+			_element = Console.ReadLine();
+		}
+		return _element;
+	}
+	public string GetElement() 
+	{
+		return _element;
+	}
+	
+	public string SetWeapon() 
+	{
+		Console.WriteLine("Choose your character's weapon : ");
+		Console.WriteLine("[Catalyst]    [Polearm]    [Sword]    [Greatsword]    [Bow]");
+		while (_weapon == "")
+		{
+			_weapon = Console.ReadLine();
+		}
+		return _weapon;
+	}
+	public string GetWeapon() 
+	{
+		return _weapon;
+	}
+	public void TestCharacter()
+	{
+		ElementalSkill(_name);
+	}
+}
--- self-train/18Nov/CobaCoba/Program.cs
+++ self-train/18Nov/CobaCoba/Program.cs
@@ -34,25 +34,213 @@
 // }
 //Object weakness
 //No type check
-using System.Collections;
-class Program
-{
-	static void Main()
-	{
-	//Fixed Size
-	//Not safety
-	int[] myArray = {1,2,3,4,5};
-	foreach (var i in myArray)
-	{
-		Console.Write($"{i} ");
-	}
-	// Type type = myArray[0].GetType();
-	// Console.WriteLine(type.Name);
-	// int a = (int)myArray[0];
-	// Console.WriteLine(a);
-	// string name = "Mahesh Chand";  
-	// Console.WriteLine("nama = {0}", name);
-	}
-}
+// using System.Collections;
+// class Program
+// {
+// 	static void Main()
+// 	{
+// 		//Fixed Size
+// 		//Not safety
+// 		int[] myArray = {1,2,3,4,5};
+// 		foreach (var i in myArray)
+// 		{
+// 			Console.Write($"{i} ");
+// 		}
+// 		string a = "200";
+// 		int aa = int.Parse(a);
+// 		int ab = Convert.ToInt32(a);
+// 		bool ac = int.TryParse(a, out int value);
+// 		if (ac == true)
+// 		{
+// 			Console.WriteLine($"value = {value}");
+// 		}
+// 		Console.WriteLine ($"aa = {aa} ab = {ab}");
+// 		int b = 200;
+// 		double ba = b; //upcasting
+// 		Console.WriteLine ($"ba = {ba}");
+		
+// 		double c = 200.89;
+// 		int ca = (int) c; //downcasting
+// 		Console.WriteLine ($"ca = {ca}");
+		
+// 		string d = "200.89";
+// 		double da = Convert.ToDouble(d);
+// 		double db = double.Parse(d);
+// 		bool dc = double.TryParse(d, out double value1);
+// 		decimal dd = (decimal) c;
+// 		Console.WriteLine ($"da = {da} db = {db} dc = {dc} dd = {dd}");
+		
+// 		Type type = myArray[0].GetType();
+// 		Console.WriteLine(type.Name);
+// 		int v = (int)myArray[0];
+// 		Console.WriteLine(v);
+// 		string name = "Mahesh Chand";  
+// 		Console.WriteLine("nama = {0}", name);
+// 	}
+// }
 
+using System;
+
+namespace DoFactory.GangOfFour.Abstract.RealWorld
+{
+    /// <summary>
+    /// MainApp startup class for Real-World
+    /// Abstract Factory Design Pattern.
+    /// </summary>
+
+    class MainApp
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+
+        public static void Main()
+        {
+            // Create and run the African animal world
+
+            ContinentFactory africa = new AfricaFactory();
+            AnimalWorld world = new AnimalWorld(africa);
+            world.RunFoodChain();
+
+            // Create and run the American animal world
+
+            ContinentFactory america = new AmericaFactory();
+            world = new AnimalWorld(america);
+            world.RunFoodChain();
+
+            // Wait for user input
+
+            Console.ReadKey();
+        }
+    }
+
+
+    /// <summary>
+    /// The 'AbstractFactory' abstract class
+    /// </summary>
+
+    abstract class ContinentFactory
+    {
+        public abstract Herbivore CreateHerbivore();
+        public abstract Carnivore CreateCarnivore();
+    }
+
+    /// <summary>
+    /// The 'ConcreteFactory1' class
+    /// </summary>
+
+    class AfricaFactory : ContinentFactory
+    {
+        public override Herbivore CreateHerbivore()
+        {
+            return new Wildebeest();
+        }
+        public override Carnivore CreateCarnivore()
+        {
+            return new Lion();
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteFactory2' class
+    /// </summary>
+
+    class AmericaFactory : ContinentFactory
+    {
+        public override Herbivore CreateHerbivore()
+        {
+            return new Bison();
+        }
+        public override Carnivore CreateCarnivore()
+        {
+            return new Wolf();
+        }
+    }
+
+    /// <summary>
+    /// The 'AbstractProductA' abstract class
+    /// </summary>
+
+    abstract class Herbivore
+    {
+    }
+
+    /// <summary>
+    /// The 'AbstractProductB' abstract class
+    /// </summary>
+
+    abstract class Carnivore
+    {
+        public abstract void Eat(Herbivore h);
+    }
+
+    /// <summary>
+    /// The 'ProductA1' class
+    /// </summary>
+
+    class Wildebeest : Herbivore
+    {
+    }
+
+    /// <summary>
+    /// The 'ProductB1' class
+    /// </summary>
+
+    class Lion : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            // Eat Wildebeest
+
+            Console.WriteLine(this.GetType().Name +
+              " eats " + h.GetType().Name);
+        }
+    }
+
+    /// <summary>
+    /// The 'ProductA2' class
+    /// </summary>
+
+    class Bison : Herbivore
+    {
+    }
+
+    /// <summary>
+    /// The 'ProductB2' class
+    /// </summary>
+
+    class Wolf : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            // Eat Bison
+
+            Console.WriteLine(this.GetType().Name +
+              " eats " + h.GetType().Name);
+        }
+    }
+
+    /// <summary>
+    /// The 'Client' class 
+    /// </summary>
+
+    class AnimalWorld
+    {
+        private Herbivore _herbivore;
+        private Carnivore _carnivore;
+
+        // Constructor
+
+        public AnimalWorld(ContinentFactory factory)
+        {
+            _carnivore = factory.CreateCarnivore();
+            _herbivore = factory.CreateHerbivore();
+        }
+
+        public void RunFoodChain()
+        {
+            _carnivore.Eat(_herbivore);
+        }
+    }
+}
 
