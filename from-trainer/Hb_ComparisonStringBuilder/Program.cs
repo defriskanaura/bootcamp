a1a16346e868adf7e575013b439ec685898c8316
Defriska <defriskanaura@gmail.com>
2023-12-07 09:06:46 +0700

    ok

--- from-trainer/Hb_ComparisonStringBuilder/Program.cs
+++ from-trainer/Hb_ComparisonStringBuilder/Program.cs
@@ -1,20 +1,35 @@
 ﻿
-using System;
-using System.Diagnostics;
-using System.Text;
+// using System;
+// using System.Diagnostics;
+// using System.Text;
 
-class Program {
-	static void Main() {
+// class Program {
+// 	static void Main() {
 		
-		StringBuilder sb = new StringBuilder();
-		Stopwatch stopwatch = new Stopwatch();
-		stopwatch.Start();
-		for (int i = 0; i < 100000; i++) {
-			sb.Append("a");
-			sb.Append("b");
-			sb.Replace('a', 'c');
-		}
-		stopwatch.Stop();
-		Console.WriteLine(stopwatch.ElapsedMilliseconds);
-	}
+// 		StringBuilder sb = new StringBuilder();
+// 		Stopwatch stopwatch = new Stopwatch();
+// 		stopwatch.Start();
+// 		for (int i = 0; i < 100000; i++) {
+// 			sb.Append("a");
+// 			sb.Append("b");
+// 			sb.Replace('a', 'c');
+// 		}
+// 		stopwatch.Stop();
+// 		Console.WriteLine(stopwatch.ElapsedMilliseconds);
+// 	}
+// }
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Notasi iterasi menggunakan loop for
+        for (int k = 8; k >= 1; k--)
+        {
+            // Lakukan sesuatu dengan nilai k
+            Console.WriteLine("Iterasi ke-" + k);
+        }
+    }
 }
+
