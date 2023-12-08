c549984a6d5ab738551f45dbd82031dba5254ca6
Defriska <defriskanaura@gmail.com>
2023-12-08 10:41:39 +0700

    ok

---
+++ on-site/Day18/R_WrapperInternalLibrary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+public static class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Starting program.");
+
+		
+		try 
+		{
+			Thread thread1 = new Thread(WrapperInternalLibrary);
+			thread1.Start();
+			thread1.Join();
+		}
+		catch(Exception e) 
+		{
+			
+		}
+		finally 
+		{
+			Console.WriteLine("End of program");
+		}
+	}
+	public static void WrapperInternalLibrary() 
+	{
+		try
+		{
+			InternalLibrary();
+		}
+		catch(Exception e) 
+		{
+			
+		}
+	}
+	public static void InternalLibrary()
+	{
+		Thread.Sleep(50);
+		ExceptionMaker();
+	}
+	public static void ExceptionMaker() 
+	{
+		throw new Exception("ini exception");
+	}
+}
