62ee4fa568f19097975a19d0c42709bed38b19c4
Defriska <defriskanaura@gmail.com>
2023-12-11 09:44:23 +0700

    ok

--- on-site/Day18/O_TaskAsyncAwait/Program.cs
+++ on-site/Day18/O_TaskAsyncAwait/Program.cs
@@ -18,3 +18,49 @@ class Program
 		//return a + b;
 	}
 }
+
+// using System;
+// using System.Data.Common;
+// using System.Threading.Tasks;
+
+// class Program
+// {
+// 	static async Task Main() 
+// 	{
+// 		// Task <-- ThreadPool 
+// 		// 	- blocking //Task.Delay //Task.Run()
+// 		// 	- non blocking // async - await
+// 		Task<int> result = Task.Run(() => AddAsync(3,4));
+// 		int myResult = await result;
+// 		Console.WriteLine(myResult);
+// 	}
+// 	static async Task<int> AddAsync(int a, int b) {
+// 		await Task.Delay(300);
+// 		return a + b;
+// 	}
+// }
+
+// using System;
+// using System.Data.Common;
+// using System.Threading.Tasks;
+
+// class Program
+// {
+// 	static async Task Main() 
+// 	{
+// 		// Task <-- ThreadPool 
+// 		// 	- blocking //Task.Delay //Task.Run()
+// 		// 	- non blocking // async - await
+// 		int test = await Task.Run(() => MyMethod());
+// 		var myResult = await AddAsync(3,4);
+// 		Console.WriteLine(myResult);
+// 	}
+// 	static async Task<int> AddAsync(int a, int b) {
+// 		await Task.Delay(300);
+// 		return a + b;
+// 	}
+// 	static int MyMethod() 
+// 	{
+// 		return 10;
+// 	}
+// }
