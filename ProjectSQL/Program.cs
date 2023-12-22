e4bfd9b5f35ef28741a68408bda3a3110876590e
Defriska Naura <defriskanaura@gmail.com>
2023-12-22 10:24:02 +0700

    ok

---
+++ ProjectSQL/Program.cs
@@ -0,0 +1,27 @@
+﻿using ProjectSQL;
+using Microsoft.EntityFrameworkCore;
+
+class Program 
+{
+	static void Main() 
+	{
+		using(Northwind db = new Northwind()) 
+		{
+			// List<Category> categories = db.Categories.Include(c => c.Products).ToList();
+			// foreach(var c in categories) 
+			// {
+			// 	Console.WriteLine(c.CategoryName);
+			// 	Console.WriteLine(c.Products.Count);
+			// 	foreach(var p in c.Products) 
+			// 	{
+			// 		Console.WriteLine("\t" + p.ProductName);
+			// 	}
+			// }
+			var products = db.Products.Where(p => p.UnitPrice > 10);
+			foreach(var p in products) 
+				{
+					Console.WriteLine("\t" + p.ProductName + " price " + p.UnitPrice);
+				}
+		}
+	}
+}
